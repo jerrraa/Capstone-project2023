59d1d6790feaf49782741522b64a30dd9213d8f4
jerrraa <104625668+jerrraa@users.noreply.github.com>
2023-03-24 15:50:20 -0300

    added code on every public page that'll create a session id and address session.

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,5 +1,27 @@
 @extends('common') 
+<?php
+//this is featured in every public page.
 
+  session_start();
+  // we'll generate a session_id if one doesn't exist by creating a unique id.
+  if (!isset($_SESSION['session_id'])) {
+    $_SESSION['session_id'] = uniqid();
+  }
+  //check if it's empty and if it is, set the session to the address.
+  if (!isset($_SESSION['ip_address'])) {
+    $_SESSION['ip_address'] = $_SERVER['REMOTE_ADDR'];
+  } else {
+    //if it's not empty, check if the address is the same as the one in the session.
+    if ($_SESSION['ip_address'] != $_SERVER['REMOTE_ADDR']) {
+      //if it's not the same, destroy the session and start a new one.
+      session_destroy();
+      session_start();
+      $_SESSION['session_id'] = uniqid();
+      $_SESSION['ip_address'] = $_SERVER['REMOTE_ADDR'];
+    }
+  }
+
+?>
 @section('pagetitle')
 Item List
 @endsection
